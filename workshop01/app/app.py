3585b7d65775405fb140fec9c577edbd27baad56
jxho <ho_junxian@hive.gov.sg>
2022-11-28 16:06:46 +0800

    initial commit

---
+++ workshop01/app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    list_quotes = [
+        "Logic will get you from A to B. Imagination will take you everywhere.",
+        "There are 10 kinds of people. Those who know binary and those who don't.",
+        "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+        "It's not that I'm so smart, it's just that I stay with problems longer.",
+        "It is pitch dark. You are likely to be eaten by a grue."
+    ]
+    img = url_for('static', filename='demo-img.jpg')
+    return render_template('index.html', quote=random.choice(list_quotes), image=img)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ workshop01/app/routes.py
@@ -0,0 +1,26 @@
+# from flask import Flask, render_template
+# 
+# app = Flask(__name__, template_folder='../pages/templates')
+# 
+# 
+# @app.route('/user/<username>')
+# def index(username):
+#     return render_template('index.html', username="username")
+# 
+# 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+#
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+#
+# @app.route(' / ')
+# def index():
+#     list_example = ['Alvin', 'Simon', 'Theodore']
+#     return render_template('index1.html', list_example=list_example)
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
